72e94191ae978350718ccf44cf945ac56294c68c
Evgeny <tungusv89@mail.ru>
2021-10-06 12:39:24 +0500

    анимация контента стр. История

--- js/script.js
+++ js/script.js
@@ -52,4 +52,672 @@ $(document).ready(function() {
             }
         }, ]
     });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(0).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(0).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(0).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(1).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(1).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(1).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(2).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(2).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(2).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(3).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(3).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(3).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(4).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(4).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(4).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(5).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(5).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(5).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(6).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(6).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(6).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(7).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(7).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(7).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(8).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(8).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(8).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(9).find('.hitem__text');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+            $element.css('position', 'interit');
+            $element.css('bottom', '0');
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+
+});
+$(document).ready(function() {
+    var $element = $('.hitem').eq(9).find('.hitem__year');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
+});
+
+$(document).ready(function() {
+    var $element = $('.hitem').eq(9).find('.history__img');
+    let counter = 0;
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop() + $(window).height();
+
+        //Если скролл до конца елемента
+        var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        // var offset = $element.offset().top
+        if (scroll > offset && counter == 0) {
+
+
+            $element.css('opacity', '1');
+            $element.css('transition', '0.5s');
+            counter = 1;
+        }
+    });
+
+
 });
